6d1a1c68127a75df1929443fb20d2d3a27132e89
Tom Needham <06needhamt@gmail.com>
2015-10-16 21:26:16 +0100

    implement basic execution unit

--- CPU/ExecutionUnit.cs
+++ CPU/ExecutionUnit.cs
@@ -2,16 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using CPU_OS_Simulator;
 
 namespace CPU_OS_Simulator.CPU
 {
-    class ExecutionUnit
+    public class ExecutionUnit
     {
-        public ExecutionUnit(SimulatorProgram program,int ClockSpeed)
-        {
+        private SimulatorProgram program;
+        private int clockSpeed;
+        private int currentIndex;
 
+        public ExecutionUnit(SimulatorProgram program, int clockSpeed)
+        {
+            this.program = program;
+            this.clockSpeed = clockSpeed;
+            this.currentIndex = 0;
         }
+        public ExecutionUnit(SimulatorProgram program, int clockSpeed, int currentIndex) : this(program, clockSpeed)
+        {
+            this.currentIndex = currentIndex;
+        }
+
+        public bool ExecuteProgram(bool step)
+        {
+            for(int i = currentIndex; i < program.Instructions.Count; i++)
+            {
+                program.Instructions.ElementAt(i).Execute();
+                Thread.Sleep(clockSpeed);
+            }
+            return true;
+        }
+        public SimulatorProgram Program
+        {
+            get
+            {
+                return program;
+            }
+
+            set
+            {
+                program = value;
+            }
+        }
+
+        public int ClockSpeed
+        {
+            get
+            {
+                return clockSpeed;
+            }
+
+            set
+            {
+                clockSpeed = value;
+            }
+        }
+
+     
     }
 }
