3ecc49b0ca7df574a059c13cc2b44b84928711fd
Tom Needham <06needhamt@gmail.com>
2015-11-15 11:51:25 +0000

    jump instructions now work with non zero base addresses

--- CPU-OS Simulator/Properties/AssemblyInfo.cs
+++ CPU-OS Simulator/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.11.15.1143")]
-[assembly: AssemblyFileVersion("2015.11.15.1143")]
+[assembly: AssemblyVersion("2015.11.15.1150")]
+[assembly: AssemblyFileVersion("2015.11.15.1150")]
 [assembly: NeutralResourcesLanguage("en-GB")]
