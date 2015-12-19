280ccaf61f12894cc343797b78c8d51780af4b2b
Tom Needham <06needhamt@gmail.com>
2015-12-19 12:21:22 +0000

    create operating system project

--- CompilerTester/Properties/AssemblyInfo.cs
+++ CompilerTester/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2015.12.19.1200")]
-[assembly: AssemblyFileVersion("2015.12.19.1200")]
+[assembly: AssemblyVersion("2015.12.19.1219")]
+[assembly: AssemblyFileVersion("2015.12.19.1219")]
--- Operating System/Properties/AssemblyInfo.cs
+++ Operating System/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
-using System.Resources;
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CPU-OS Simulator Unit Test Module")]
-[assembly: AssemblyDescription("CPU-OS Simulator Unit Test Module")]
+[assembly: AssemblyTitle("CPU-OS Simulator Operating System Module")]
+[assembly: AssemblyDescription("CPU-OS Simulator Operating System Module")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Edge Hill University UK")]
 [assembly: AssemblyProduct("CPU-OS Simulator")]
@@ -14,24 +15,25 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c2d06e1-23f6-49d7-b283-6457efcb0961")]
+[assembly: Guid("33ba51f9-d0a8-4e7d-81b9-d948c002ae85")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2015.12.19.1219")]
 [assembly: AssemblyFileVersion("2015.12.19.1219")]
 [assembly: NeutralResourcesLanguage("en-GB")]
+
