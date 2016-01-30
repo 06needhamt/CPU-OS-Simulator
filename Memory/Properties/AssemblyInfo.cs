2a7b051b2b5ccfc27df12d7cd81ab23e29fe1f74
Tom Needham <06needhamt@gmail.com>
2016-01-30 12:46:49 +0000

    Readded accidentally deleted file

--- Memory/Properties/AssemblyInfo.cs
+++ Memory/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.1.30.1240")]
-[assembly: AssemblyFileVersion("2016.1.30.1240")]
+[assembly: AssemblyVersion("2016.1.30.1245")]
+[assembly: AssemblyFileVersion("2016.1.30.1245")]
 [assembly: NeutralResourcesLanguage("en-GB")]
