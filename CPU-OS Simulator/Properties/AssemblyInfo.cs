404707a440dc08538858f4dfeef81d1ba77ec9d5
Tom Needham <06needhamt@gmail.com>
2016-03-07 18:55:52 +0000

    Re-add XWT binaries

--- CPU-OS Simulator/Properties/AssemblyInfo.cs
+++ CPU-OS Simulator/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.3.6.0031")]
-[assembly: AssemblyFileVersion("2016.3.6.0031")]
+[assembly: AssemblyVersion("2016.3.7.1853")]
+[assembly: AssemblyFileVersion("2016.3.7.1853")]
 [assembly: NeutralResourcesLanguage("en-GB")]
--- CompilerTester/Properties/AssemblyInfo.cs
+++ CompilerTester/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.3.6.0011")]
-[assembly: AssemblyFileVersion("2016.3.6.0011")]
+[assembly: AssemblyVersion("2016.3.7.1853")]
+[assembly: AssemblyFileVersion("2016.3.7.1853")]
